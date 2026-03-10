cd8036bba6bd2ddc3169fe90da2f88adce990570
Leo_admin <310-Teacher@514-4.spbstu.ru>
2026-03-10 12:57:11 +0300

    Создан файл

---
+++ Nikolenko_project-main/task_01_passoport.py
@@ -0,0 +1,19 @@
+student_name = "Николенко Анна Александровна"
+group_number = "3140801/52501"
+project_name = "Янино Форест"
+floors = "9"
+height = "28.5"
+is_residential = True
+construction_year = "2027"
+
+print("===ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА===")
+print(f"Составитель: {student_name}")
+print(f"Группа: {group_number}")
+print(f"Объект: {project_name}")
+print(f"Этажность: {floors}")
+print(f"Высота: {height}")
+if is_residential:
+    print("Тип: жилое")
+else:
+    print("Тип: нежилое")
+print(f"Год постройки: {construction_year}")
